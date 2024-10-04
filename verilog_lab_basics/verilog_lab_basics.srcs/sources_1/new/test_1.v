2d2352a69aee904e831083353d3f5e455c459b59
Harish Kumar <g.harishkumar1998@gmail.com>
2024-10-04 10:40:39 +0530

    from CDAC Lab

---
+++ verilog_lab_basics/verilog_lab_basics.srcs/sources_1/new/test_1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/04/2024 10:39:43 AM
+// Design Name: 
+// Module Name: test_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_1(
+
+    );
+endmodule
